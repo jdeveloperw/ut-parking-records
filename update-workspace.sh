241ff716c0f4bfea51985ab6ccbd9ec6b07aec81
JD White <jdeveloperw@gmail.com>
2013-12-27 14:36:23 -0800

    first version of ansible setup

--- update-workspace.sh
+++ update-workspace.sh
@@ -1,3 +1,3 @@
 #sudo pip install -r pip-requirements.txt
 #./git-config.sh
-ansible-playbook -i ./ansible-hosts.txt playbook.yml -K
+ansible-playbook -i ./ansible/ansible-hosts.txt ansible/playbook.yml -K
