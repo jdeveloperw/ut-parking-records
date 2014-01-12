ee78c83dc832e7c7dbfccdf00c64bb50a4b08661
JD White <jdeveloperw@gmail.com>
2014-01-11 20:10:39 -0800

    basic form validation for number of permits working

--- ui/app/js/controllers.js
+++ ui/app/js/controllers.js
@@ -2,10 +2,23 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+angular.module('myApp.controllers', [])
+  .controller('InputFormController', function($scope) {
+    $scope.parking_record = {
+      number_of_permits: 0,
+      has_placard: false,
+      is_handicapped: false,
+      is_wheel_chair_accessible: false,
+      is_return: false,
+      year: 2007,
+      first_permit_number: 0,
+      last_permit_number: 0,
+      lot: 123,
+      type: 123,
+      payment_type: 123,
+      sport: 'football!',
+      department: 'nuclear engineering',
+      person: 'Ashly!'
+    }
+  })
+  ;
--- ui/app/js/directives.js
+++ ui/app/js/directives.js
@@ -2,10 +2,51 @@
 
 /* Directives */
 
+var INTEGER_REGEXP = /^\-?\d+$/;
+var NON_NEGATIVE_INTEGER_REGEXP = /^\d+$/;
+var POSITIVE_INTEGER = /^[1..9]\d*$/;
 
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
+angular.module('myApp.directives', [])
+  .directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]);
+  }])
+  /* TODO DRY */
+  .directive('nonNegativeInteger', function() {
+    return {
+      require: 'ngModel',
+      link: function(scope, elm, attrs, ctrl) {
+        ctrl.$parsers.unshift(function(viewValue) {
+          if (NON_NEGATIVE_INTEGER_REGEXP.test(viewValue)) {
+            // it is valid
+            ctrl.$setValidity('nonNegativeInteger', true);
+            return viewValue;
+          } else {
+            // it is invalid, return undefined (no model update)
+            ctrl.$setValidity('nonNegativeInteger', false);
+            return undefined;
+          }
+        });
+      }
+    };
+  })
+  .directive('positiveInteger', function() {
+    return {
+      require: 'ngModel',
+      link: function(scope, elm, attrs, ctrl) {
+        ctrl.$parsers.unshift(function(viewValue) {
+          if (NON_NEGATIVE_INTEGER_REGEXP.test(viewValue)) {
+            // it is valid
+            ctrl.$setValidity('positiveInteger', true);
+            return viewValue;
+          } else {
+            // it is invalid, return undefined (no model update)
+            ctrl.$setValidity('positiveInteger', false);
+            return undefined;
+          }
+        });
+      }
+    };
+  })
+  ;
