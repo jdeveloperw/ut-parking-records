1478cea23cf24d8e7cccc799aa0f944b176f75ee
JD White <jdeveloperw@gmail.com>
2014-01-12 16:17:39 -0800

    Move new parking record creation to a service

--- ui/app/js/controllers.js
+++ ui/app/js/controllers.js
@@ -4,22 +4,7 @@
 
 angular.module('myApp.controllers', ['myApp.services'])
   .controller('InputFormController', function($scope) {
-    $scope.parking_record = {
-      number_of_permits: 0,
-      has_placard: false,
-      is_handicapped: false,
-      is_wheel_chair_accessible: false,
-      is_return: false,
-      year: 2007,
-      first_permit_number: 0,
-      last_permit_number: 0,
-      lot: 'CG',
-      type: 123,
-      payment_type: 123,
-      sport: 'football!',
-      department: 'nuclear engineering',
-      person: 'Ashly!'
-    };
+    $scope.parking_record = ParkingRecords.new();
   })
   .controller('YearController', function($scope, min_year, max_year) {
     $scope.min_year = min_year;
--- ui/app/js/services.js
+++ ui/app/js/services.js
@@ -2,6 +2,35 @@
 
 /* Services */
 
+var Lots = {
+  all: function() {
+    return [ 'AA', 'CG', 'GT' ];
+  }
+}
+
+
+var ParkingRecords = {
+  new: function() {
+    return {
+      number_of_permits: 0,
+      has_placard: false,
+      is_handicapped: false,
+      is_wheel_chair_accessible: false,
+      is_return: false,
+      year: 2007,
+      first_permit_number: 0,
+      last_permit_number: 0,
+      lot: 'CG',
+      type: 123,
+      payment_type: 123,
+      sport: 'football!',
+      department: 'nuclear engineering',
+      person: 'Ashly!'
+    }
+  }
+}
+
+
 angular.module('myApp.services', [])
   .value('version', '0.1')
   // TODO Is the the correct way?
@@ -13,10 +42,9 @@ angular.module('myApp.services', [])
   .value('min_permit_number', 0)
   .value('max_permit_number', 1000)
   .service('Lots', function() {
-    return {
-      all: function() {
-        return [ 'AA', 'CG', 'GT' ];
-      }
-    }
+    return Lots;
+  })
+  .service('ParkingRecords', function() {
+    return ParkingRecords;
   })
   ;
