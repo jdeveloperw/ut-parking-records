8c94160d5c751a7320015c268df577c2b9935477
JD White <jdeveloperw@gmail.com>
2014-01-17 22:30:02 -0800

    remove a lot of directives and tests that are no longer needed

--- ui/app/js/directives.js
+++ ui/app/js/directives.js
@@ -2,10 +2,6 @@
 
 /* Directives */
 
-var INTEGER_REGEXP = /^\-?\d+$/;
-var NON_NEGATIVE_INTEGER_REGEXP = /^\d+$/;
-var POSITIVE_INTEGER = /^[1..9]\d*$/;
-
 var createValidatorLink = function(directiveName, isValid) {
   return function(scope, elm, attrs, ctrl) {
     ctrl.$parsers.unshift(function(viewValue) {
@@ -46,20 +42,6 @@ angular.module('myApp.directives', ['myApp.services'])
           return _.contains(scope.container, viewValue);
       })
     };
-  })
-  .directive('nonNegativeInteger', function() {
-    return RegExpValidator('nonNegativeInteger', NON_NEGATIVE_INTEGER_REGEXP);
-  })
-  .directive('positiveInteger', function() {
-    return RegExpValidator('positiveInteger', POSITIVE_INTEGER);
-  })
-  .directive('validYear', function(minYear, maxYear) {
-    return IntegerRangeValidator('validYear', minYear, maxYear);
-  })
-  .directive('validPermitNumber', function(minPermitNumber, maxPermitNumber) {
-    return IntegerRangeValidator('validPermitNumber',
-                                 minPermitNumber,
-                                 maxPermitNumber);
   })
   .directive('validLot', function(Lots) {
     return ContainmentValidator('validLot', Lots.all);
